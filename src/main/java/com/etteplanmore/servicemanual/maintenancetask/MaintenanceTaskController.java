c3570d2d121bb5bc73a77f2741f66dbe2310d303
Valtteri Valtanen <vade1997@gmail.com>
2020-01-25 17:53:50 +0200

    delete mapping for maintenance tasks

--- src/main/java/com/etteplanmore/servicemanual/maintenancetask/MaintenanceTaskController.java
+++ src/main/java/com/etteplanmore/servicemanual/maintenancetask/MaintenanceTaskController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -79,4 +80,12 @@ public class MaintenanceTaskController {
 		return "Updated";
 	}
 	
+	@DeleteMapping("/maintenancetasks/{id}")
+	String deleteMaintenanceTask(@PathVariable Long id) {
+		if (!repository.existsById(id)) {
+			throw new MaintenanceTaskNotFoundException(id);
+		}
+		repository.deleteById(id);
+		return "Deleted";
+	}
 }
