c6fd45566bf3ffc8282dbfe0580e3e89ce44d7dc
Valtteri Valtanen <vade1997@gmail.com>
2020-01-26 19:40:25 +0200

    added endpoint to initialize some factory devices for testing

--- src/main/java/com/etteplanmore/servicemanual/factorydevice/FactoryDeviceController.java
+++ src/main/java/com/etteplanmore/servicemanual/factorydevice/FactoryDeviceController.java
@@ -1,6 +1,10 @@
 package com.etteplanmore.servicemanual.factorydevice;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 
@@ -22,5 +26,23 @@ public class FactoryDeviceController {
     FactoryDevice one(@PathVariable Long id) {
         return repository.findById(id)
             .orElseThrow(() -> new FactoryDeviceNotFoundException(id));
+    }
+    
+    @GetMapping("/factorydevices/init")
+    String init() {
+    	final List<FactoryDevice> devices = Arrays.asList(
+                new FactoryDevice("Device X", 2001, "type 10"),
+                new FactoryDevice("Device Y", 2012, "type 3"),
+                new FactoryDevice("Device Z", 1985, "type 1"),
+                new FactoryDevice("Device 0", 2004, "type 19"),
+                new FactoryDevice("Device 1", 1987, "type 12"),
+                new FactoryDevice("Device 2", 1982, "type 11"),
+                new FactoryDevice("Device 3", 1995, "type 5"),
+                new FactoryDevice("Device 4", 1990, "type 2"),
+                new FactoryDevice("Device 5", 1982, "type 1"),
+                new FactoryDevice("Device T", 2001, "type 10")
+            );
+    	repository.saveAll(devices);
+    	return "Some factory devices added";
     }
 }
